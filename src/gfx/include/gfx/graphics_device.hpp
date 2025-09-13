3c6b16455698f8bbde5e6c1d7799fd965358fb21
klop51 <braulio.hernani@hotmail.com>
2025-09-13 07:30:44 +0200

    Fix include path in graphics_device.hpp
    - Change relative path to graphics_device_bridge.hpp to use correct directory structure
    - Resolves compilation error: 'graphics_device_bridge.hpp' file not found

--- src/gfx/include/gfx/graphics_device.hpp
+++ src/gfx/include/gfx/graphics_device.hpp
@@ -3,7 +3,7 @@
 
 #pragma once
 
-#include "graphics_device_bridge.hpp"
+#include "../../../graphics_device_bridge.hpp"
 
 // The GraphicsDevice class is already available through the bridge header
 // No need to re-export as it would create circular reference
